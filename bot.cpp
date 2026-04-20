4b2e72d857104ad321937992e369085d7fb15263
kolomoecal1 <joncarlson970@gmail.com>
2026-04-20 12:26:58 +0300

    the assembly is completed. attempt - 1

--- bot.cpp
+++ bot.cpp
@@ -1,12 +1,6 @@
-//#include "bot.h"
-//#include <cstdlib>
-//#include <ctime>
-//
-//std::pair<Dot, Dir> Bot::place()
-//{
-//	FIELD::Directions dir_name = FIELD::Directions(rand() % FIELD::Directions::COUNT);
-//	Dir dir = FIELD::dirs[dir_name];
-//	Dot dot{ rand() % FIELD::SIZE, rand() % FIELD::SIZE };
-//
-//	return std::pair<Dot, Dir>(dot, dir);
-//}
+#include "bot.h"
+
+Bot::~Bot()
+{
+  
+}
